701e226dadf047bfb09d22545eadcad1ceab20ce
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-04 13:15:24 -0600

    Sort words in to difficulty levels

---
+++ server/filterWords.js
@@ -0,0 +1,25 @@
+const words = (await Bun.file("./words.txt").text()).split("\n");
+
+const difficulties = {
+  1: [],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+  6: [],
+  7: [],
+  8: [],
+  9: [],
+  10: [],
+};
+
+for (let word of words) {
+  let difficulty = Math.min(Math.max(1, Math.max(1, word.length - 4)), 10);
+  if (difficulty > 10 || difficulty == 0) {
+    console.log(word, difficulty);
+    continue;
+  }
+  difficulties[difficulty].push(word);
+}
+
+Bun.write("./words.json", JSON.stringify(difficulties, null, 4));
