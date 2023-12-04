30918a8089eb7804e54d91babaf3cf54c25c9921
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-04 15:05:17 -0600

    Add words query Add graphql-parse-resolve-info package

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,24 @@
-const resolvers = {};
+import { Word } from "../models/index.js";
+import { parseResolveInfo } from "graphql-parse-resolve-info";
+
+const resolvers = {
+  Query: {
+    words: async (parent, { difficulty }, context, info) => {
+      const ast = parseResolveInfo(info);
+      const fields = {};
+      for (const field in ast.fieldsByTypeName.Word) {
+        fields[field] = 1;
+      }
+      return Word.find(
+        {
+          difficulty: {
+            $lte: difficulty,
+          },
+        },
+        fields
+      ).limit(50);
+    },
+  },
+};
 
 export default resolvers;
