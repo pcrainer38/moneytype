1895882cdd8563962c421a46385aad9442123f24
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-05 13:37:57 -0600

    Change leaderboard to return 15 instead of 25

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -47,12 +47,13 @@ const resolvers = {
       ).limit(50);
     },
     leaderboard: async (parent, {page}, context, info) => {
+      const usersPerPage = 15;
       const ast = parseResolveInfo(info);
       const fields = {};
       for (const field in ast.fieldsByTypeName.PartialUser) {
         fields[field] = 1;
       }
-      return User.find({}, fields).sort({virtualMoney: -1}).limit(25).skip(page * 25);
+      return User.find({}, fields).sort({virtualMoney: -1}).limit(usersPerPage).skip(page * usersPerPage);
     }
   },
   
