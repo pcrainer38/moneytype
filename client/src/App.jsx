c1b35fb1e26a7960bf2a5e95692992c9ae8fab37
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-05 14:40:44 -0600

    Use new navlink component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
-import { Outlet } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
-import { Link } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import NavLink from "./components/NavLink.jsx";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 const client = new ApolloClient({
-  uri: '/graphql',
+  uri: "/graphql",
   cache: new InMemoryCache(),
 });
 
@@ -15,22 +15,18 @@ function App() {
     <ApolloProvider client={client}>
       <Navbar expand="lg" id="header">
         <Container>
-              <h1>Money Type</h1>
-              <nav>
-                <Link to={"/leaderboard"}>
-                  <button as="input" type='button' className='navbtn'>leaderboard</button>
-                </Link>
-                <Link to={`/signUp`} >
-                  <button as="input" type='button' className='navbtn'>Sign Up</button>
-                </Link>
-              </nav>
+          <h1>Money Type</h1>
+          <nav>
+            <NavLink to={"/leaderboard"}>Leaderboard</NavLink>
+            <NavLink to={`/signUp`}>Sign Up</NavLink>
+          </nav>
         </Container>
       </Navbar>
-        <div className='gameCard'>
-          <Outlet />
-        </div>
+      <div className="gameCard">
+        <Outlet />
+      </div>
     </ApolloProvider>
-  )
+  );
 }
 
-export default App
+export default App;
