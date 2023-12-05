ee5173549bde57f4a06d3943ba1dd59d4354809f
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-05 14:40:30 -0600

    Capitalize Toggle Theme

--- client/src/pages/game.jsx
+++ client/src/pages/game.jsx
@@ -1,52 +1,56 @@
 // import { bootstrap } from 'bootstrap';
 // import { useQuery } from '@apollo/client';
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
 const Game = () => {
-
-  
-    
-  
-    return (
+  return (
     <>
-        <Container>
-            <div className='gameWindow d-flex justify-content-between'>
-                <div className='wordCard d-flex align-items-center justify-content-center w-75'>
-                    <p id='Word'>word</p>
-                </div>
-                <div className='UpgradesCard'>
-                    <h3>upgrades</h3>
-                    
-                    <div className='upgrades d-flex'>
-                        <div className='icons'>
-                            <ul>
-                                <li><img></img></li>
-                                <li><img></img></li>
-                                <li><img></img></li>
-                                <li><img></img></li>
-                            </ul>
-                        </div>
-                        <ul id="upgradelist">
-                            {/* {upgradeschema.map((upgrade) => {
+      <Container>
+        <div className="gameWindow d-flex justify-content-between">
+          <div className="wordCard d-flex align-items-center justify-content-center w-75">
+            <p id="Word">word</p>
+          </div>
+          <div className="UpgradesCard">
+            <h3>upgrades</h3>
+
+            <div className="upgrades d-flex">
+              <div className="icons">
+                <ul>
+                  <li>
+                    <img></img>
+                  </li>
+                  <li>
+                    <img></img>
+                  </li>
+                  <li>
+                    <img></img>
+                  </li>
+                  <li>
+                    <img></img>
+                  </li>
+                </ul>
+              </div>
+              <ul id="upgradelist">
+                {/* {upgradeschema.map((upgrade) => {
                                 <li key={upgrade._id} value={upgrade.name}>
                                     {upgrade._id}
                                 </li>  
                             })} */}
-                            <li>upgrade1</li>
-                            <li>upgrade2</li>
-                            <li>upgrade3</li>
-                            <li>upgrade4</li>
-                        </ul>              
-                    </div>
-                </div>
+                <li>upgrade1</li>
+                <li>upgrade2</li>
+                <li>upgrade3</li>
+                <li>upgrade4</li>
+              </ul>
             </div>
-        </Container>
-        <Container>
-            <div className='toggle d-flex justify-content-end'>
-                <button>toggle theme</button>
-            </div>
-        </Container>
+          </div>
+        </div>
+      </Container>
+      <Container>
+        <div className="toggle d-flex justify-content-end">
+          <button>Toggle Theme</button>
+        </div>
+      </Container>
     </>
-    );
-  };
+  );
+};
 
-  export default Game;
+export default Game;
