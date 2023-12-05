f8695115553445205d689499ea46b0fef8dbca87
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-05 14:44:41 -0600

    Fix NavLink button props

--- client/src/components/NavLink.jsx
+++ client/src/components/NavLink.jsx
@@ -3,7 +3,7 @@ import { NavLink as RouterNavLink } from "react-router-dom";
 function NavLink({ to, children }) {
   return (
     <RouterNavLink to={to} className="navbtn">
-      {(isActive, isPending, isTransitioning) => (
+      {({ isActive, isPending, isTransitioning }) => (
         <button
           as="input"
           type="button"
