ab8bc2621625c1a31a8f4c44bfe3cf34060a62fb
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-06 12:12:28 -0600

    Add wordDifficulty upgrade to gameLogic.js

--- server/utils/gameLogic.js
+++ server/utils/gameLogic.js
@@ -6,6 +6,13 @@ const upgrades = {
       return this.baseCost * (1 + level ** 2);
     },
   },
+  wordDifficulty: {
+    baseCost: 250,
+    maxLevel: 10,
+    calculateCost(level) {
+      return this.baseCost + (level - 1) * 750;
+    },
+  },
 };
 
 export const getUpgradeCost = (upgrade, level) => {
