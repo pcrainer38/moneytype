f98e14824d811b5ba35d0c7c8a0efd39f22b343f
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-06 13:08:54 -0600

    Update static file path in production environment

--- server/index.js
+++ server/index.js
@@ -31,10 +31,10 @@ app.use(
 );
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../../client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
   });
 }
 
