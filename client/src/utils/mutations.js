d2c1202827fa9d0fdaab95894448841546c0b1c5
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-07 10:27:20 -0600

    Fix login and addUser mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,29 +1,13 @@
-import {gql} from "@apollo/client"
-
-
+import { gql } from "@apollo/client";
 
 export const ADD_USER = gql`
-mutation Mutation($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        email
-        username
-      }
-    }
+  mutation Mutation($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password)
   }
-`
+`;
 
 export const LOGIN_USER = gql`
-mutation Mutation($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        email
-        username
-      }
-    }
+  mutation Mutation($email: String!, $password: String!) {
+    login(email: $email, password: $password)
   }
-`
+`;
