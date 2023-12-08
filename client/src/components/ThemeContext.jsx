6bbe055574e0dd9218be3ef8e202ecc940580e3d
Andrew Shoener <a.b.shoener@gmail.com>
2023-12-08 11:02:50 -0600

    ThemeContext now loads theme from database

--- client/src/components/ThemeContext.jsx
+++ client/src/components/ThemeContext.jsx
@@ -1,11 +1,27 @@
-import { createContext, useContext, useState } from "react";
+import { useMutation, useQuery } from "@apollo/client";
+import { createContext, useContext, useRef, useState } from "react";
+import { GET_SETTINGS } from "../utils/queries.js";
+import { UPDATE_SETTINGS } from "../utils/mutations.js";
 
 const ThemeContext = createContext();
 
 export const useThemeContext = () => useContext(ThemeContext);
 
 function ThemeProvider(props) {
-  const [theme, setTheme] = useState("light");
+  const hasSetTheme = useRef(false);
+  const { loading, data, previousData } = useQuery(GET_SETTINGS);
+  const [updateSettings] = useMutation(UPDATE_SETTINGS);
+  const [theme, setThemeInternal] = useState("light");
+
+  function setTheme(theme) {
+    updateSettings({ variables: { theme } });
+    setThemeInternal(theme);
+  }
+
+  if (!loading && !hasSetTheme.current) {
+    setThemeInternal(data.userSettings.theme);
+    hasSetTheme.current = true;
+  }
 
   return <ThemeContext.Provider value={{ theme, setTheme }} {...props} />;
 }
